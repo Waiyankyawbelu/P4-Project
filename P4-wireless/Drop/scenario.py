db6ec5ab09138a9a67a58935ed792a31ab64a668
Wai Yan Kyaw <waiyankyawbelu@gmail.com>
2024-01-04 17:07:16 -0500

    Updated Drop

--- P4-wireless/Drop/scenario.py
+++ P4-wireless/Drop/scenario.py
@@ -18,7 +18,7 @@ from mn_wifi.bmv2 import P4AP, P4Host
 from mininet.node import RemoteController
 
 
-def topology(remote_controller):
+def topology():
     'Create a network.'
     net = Mininet_wifi()
 
@@ -28,23 +28,11 @@ def topology(remote_controller):
     sta1 = net.addStation('sta1', ip='192.168.1.1', mac="00:00:00:00:00:03")
     sta2 = net.addStation('sta2', ip='192.168.2.1', mac="00:00:00:00:00:04")
 
-    args1 = dict()
-    args2 = dict()
-    if not remote_controller:
-        path = os.path.dirname(os.path.abspath(__file__))
-        json_file = path + '/ap-runtime.json'
-        config1 = path + '/commands_ap1.txt'
-        config2 = path + '/commands_ap2.txt'
-        args1 = {'json': json_file, 'switch_config': config1}
-        args2 = {'json': json_file, 'switch_config': config2}
-
     info('*** Adding P4APs\n')
-    ap1 = net.addAccessPoint('ap1', cls=P4AP, netcfg=True, **args1)
-    ap2 = net.addAccessPoint('ap2', cls=P4AP, netcfg=True, **args2)
-
-    if remote_controller:
-        info('*** Adding Controller\n')
-        net.addController('c0', controller=RemoteController)
+    ap1 = net.addAccessPoint('ap1', cls=P4AP, json_path='build/drop.json', runtime_json_path='runtime/ap1-runtime.json', 
+			     log_console = True, log_dir = os.path.abspath('logs'), log_file = 'ap1.log', pcap_dump = os.path.abspath('pcaps'))
+    ap2 = net.addAccessPoint('ap2', cls=P4AP, json_path='build/drop.json', runtime_json_path='runtime/ap2-runtime.json', 
+			     log_console = True, log_dir = os.path.abspath('logs'), log_file = 'ap2.log', pcap_dump = os.path.abspath('pcaps'))
 
     net.configureWifiNodes()
 
@@ -56,8 +44,6 @@ def topology(remote_controller):
 
     info('*** Starting network\n')
     net.start()
-    if not remote_controller:
-        net.staticArp()
 
     info('*** Running CLI\n')
     CLI(net)
@@ -68,5 +54,4 @@ def topology(remote_controller):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    remote_controller = True if '-r' in sys.argv else False
-    topology(remote_controller)
+    topology()
