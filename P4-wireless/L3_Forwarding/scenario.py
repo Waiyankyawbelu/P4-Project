723fbf6e3256002bc413764dd35a3920c75171a3
Wai Yan Kyaw <waiyankyawbelu@gmail.com>
2024-01-06 16:25:53 -0500

    Updated code

--- P4-wireless/L3_Forwarding/scenario.py
+++ P4-wireless/L3_Forwarding/scenario.py
@@ -8,10 +8,9 @@ from mininet.log import setLogLevel, info
 from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.bmv2 import P4AP, P4Host
-from mininet.node import RemoteController
 
 
-def topology(remote_controller):
+def topology():
     'Create a network.'
     net = Mininet_wifi()
 
@@ -20,27 +19,19 @@ def topology(remote_controller):
     sta2 = net.addStation('sta2', ip='192.168.2.2', mac='08:00:00:00:02:22')
     sta3 = net.addStation('sta3', ip='192.168.3.3', mac='08:00:00:00:03:33')
 
-    args1 = dict()
-    args2 = dict()
-    args3 = dict()
-    if not remote_controller:
-        path = os.path.dirname(os.path.abspath(__file__))
-        json_file = path + '/build/l3_forwarding.json'
-        config1 = path + '/build/ap1-runtime.json'
-        config2 = path + '/build/ap2-runtime.json'
-        config3 = path + '/build/ap3-runtime.json'
-        args1 = {'json': json_file, 'switch_config': config1}
-        args2 = {'json': json_file, 'switch_config': config2}
-        args3 = {'json': json_file, 'switch_config': config3}
 
-    info('*** Adding P4APs\n')
-    ap1 = net.addAccessPoint('ap1', cls=P4AP, netcfg=True, **args1)
-    ap2 = net.addAccessPoint('ap2', cls=P4AP, netcfg=True, **args2)
-    ap3 = net.addAccessPoint('ap3', cls=P4AP, netcfg=True, **args3)
+    info('*** Adding P4RuntimeAP\n')
+    ap1 = net.addAccessPoint('ap1',  cls=P4AP, json_path='build/l3_forwarding.json', runtime_json_path='runtime/ap1-runtime.json', 
+			     log_console = True, log_dir = os.path.abspath('logs'), log_file = 'ap1.log', pcap_dump = os.path.abspath('pcaps'))
 
-    if remote_controller:
-        info('*** Adding Controller\n')
-        net.addController('c0', controller=RemoteController)
+    ap2 = net.addAccessPoint('ap2',  cls=P4AP, json_path='build/l3_forwarding.json', runtime_json_path='runtime/ap2-runtime.json', 
+			     log_console = True, log_dir = os.path.abspath('logs'), log_file = 'ap2.log', pcap_dump = os.path.abspath('pcaps'))
+    
+    ap3 = net.addAccessPoint('ap3',  cls=P4AP, json_path='build/l3_forwarding.json', runtime_json_path='runtime/ap3-runtime.json', 
+			     log_console = True, log_dir = os.path.abspath('logs'), log_file = 'ap3.log', pcap_dump = os.path.abspath('pcaps'))
+
+
+
 
     net.configureWifiNodes()
 
@@ -54,8 +45,7 @@ def topology(remote_controller):
 
     info('*** Starting network\n')
     net.start()
-    if not remote_controller:
-        net.staticArp()
+    net.staticArp()
 
     info('*** Running CLI\n')
     CLI(net)
@@ -66,5 +56,4 @@ def topology(remote_controller):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    remote_controller = True if '-r' in sys.argv else False
-    topology(remote_controller)
+    topology()
